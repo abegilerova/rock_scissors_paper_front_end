0725ec42b3f9e5da962b3d198e6a6efd40199b73
abegilerova <abegilerova@gmail.com>
2020-09-29 22:10:07 -0500

    react strap

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Button } from 'reactstrap'
 
 function UserProfile(props) {
   return (
     <div className="user-profile">
       <h1>The username is {props.username}</h1>
+      <h1>The number of games they've won so far is {props.won_games}</h1>
+      <h1>The number of games they've lost so far is {props.lost_games}</h1>
+      <Button>Play New Game</Button>
+
 
     </div>
   );
